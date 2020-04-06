81f35185e77488b9d497d903ec1305ff0a64bb49
myotherbrother <darrel.gill@outlook.com>
2020-04-06 14:55:33 -0600

    refactor code by adding helper methods

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 class Settings:
     """ A class to store all settng for Alian Invasion."""
 
@@ -9,3 +10,6 @@ class Settings:
         self.screen_width = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
+
+        # Ship settings
+        self.ship_speed = 1.5
--- ship.py
+++ ship.py
@@ -10,6 +10,7 @@ class Ship:
         """Initialize the ship and set its starting position."""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
+        self.settings = ai_game.settings
 
         # Load the ship image and get its rect.
 
@@ -18,6 +19,26 @@ class Ship:
 
         # Start each new ship at the bottom center of the screen.
         self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a decimal value for the ship's horizontal position.
+        self.x = float(self.rect.x)
+
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
+
+    def update(self):
+        """Update the ship's position based on the movement flag"""
+        # Update the ship's x value, not the rect.
+
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # Update rect object from self.x.
+        self.rect.x = self.x
 
     def blitme(self):
         """blitme will draw the image to the screen at the self.rect postion"""
