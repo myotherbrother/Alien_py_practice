604056f9d0b026afcbd92c2cefcbb3a551e5f41b
myotherbrother <darrel.gill@outlook.com>
2020-04-06 13:00:52 -0600

    create setting.py and add ship image

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,7 +2,7 @@
 
 import sys
 import pygame
-
+from settings import Settings
 
 class AlienInvasion:
     """Overall class to manage game assets and behaviour."""
@@ -11,8 +11,13 @@ class AlienInvasion:
         """Initialize the game, and create game resources."""
 
         pygame.init()
-        self.screen = pygame.display.set_mode((1200, 800))
-        pygame.display.set_caption("Pandemic 2020")
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,
+self.settings.screen_height))
+        pygame.display.set_caption("Alien Invasion Pandemic 2020")
+        
+   
+
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -23,6 +28,8 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            #Redraw the screen during each pass through the loop.
+            self.screen.fill(self.settings.bg_color)
 
             # Make the most recently drawn screen visible.
 
---
+++ settings.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+class Settings:
+    """ A class to store all settng for Alian Invasion."""
+
+    def __init__(self):
+        """Initialize game settings."""
+        # Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
