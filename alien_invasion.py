b0f2fbaa1f77f894e7af7cbd22a5e07d702579ad
myotherbrother <darrel.gill@outlook.com>
2020-04-06 13:37:27 -0600

    bind ship object to game instance

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,8 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
+
 
 class AlienInvasion:
     """Overall class to manage game assets and behaviour."""
@@ -12,12 +14,10 @@ class AlienInvasion:
 
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((self.settings.screen_width,
-self.settings.screen_height))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion Pandemic 2020")
-        
-   
-
+        self.ship = Ship(self)
 
     def run_game(self):
         """Start the main loop for the game."""
@@ -28,8 +28,9 @@ self.settings.screen_height))
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-            #Redraw the screen during each pass through the loop.
+            # Redraw the screen during each pass through the loop.
             self.screen.fill(self.settings.bg_color)
+            self.ship.blitme()
 
             # Make the most recently drawn screen visible.
 
