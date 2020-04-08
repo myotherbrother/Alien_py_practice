2ea255867cb2a4165766ef88be2729cb0afefaf4
myotherbrother <darrel.gill@outlook.com>
2020-04-08 15:44:47 -0600

    Remove mouse from screen duging game play

--- alien_invasion.py
+++ alien_invasion.py
@@ -60,8 +60,25 @@ class AlienInvasion:
 
     def _check_play_button(self, mouse_pos):
         """start a new game when the play button is clicked"""
-        if self.play_button.rect.collidepoint(mouse_pos):
+        button_clicked = self.play_button.rect.collidepoint(mouse_pos)
+        if button_clicked and not self.stats.game_active:
+            # Reset the game statistics.
+            self.stats.reset_stats()
             self.stats.game_active = True
+
+            # Get rid of any remaining aliens and bullets.
+
+            self.aliens.empty()
+            self.bullets.empty()
+
+            # Create a new fleet and center the ship.
+
+            self._create_fleet()
+            self.ship.center_ship()
+
+            # Hide the mouse cursor.
+
+            pygame.mouse.set_visible(False)
 
     def _check_keydown_events(self, event):
         """Respond to key pressess."""
@@ -150,6 +167,7 @@ class AlienInvasion:
             sleep(0.5)
         else:
             self.stats.game_active = False
+            pygame.mouse.set_visible(True)
 
     def _create_fleet(self):
         """Create the fleet of aliens"""
