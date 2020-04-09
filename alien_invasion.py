18d2aec550f97d33aeb8d3eeef379dfa68be5b80
myotherbrother <darrel.gill@outlook.com>
2020-04-08 22:33:51 -0600

    add level indicator

--- alien_invasion.py
+++ alien_invasion.py
@@ -70,6 +70,7 @@ class AlienInvasion:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
 
             # Get rid of any remaining aliens and bullets.
 
@@ -130,6 +131,7 @@ class AlienInvasion:
             for aliens in collisions.values():
                 self.stats.score += self.settings.alien_points * len(aliens)
             self.sb.prep_score()
+            self.sb.check_high_score()
 
         if not self.aliens:
             # Destroy exisitng bullets and create new fleet.
@@ -137,6 +139,10 @@ class AlienInvasion:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level.
+            self.stats.level += 1
+            self.sb.prep_level()
 
     def _update_aliens(self):
         """Update the positions of all aliens in the fleet"""
--- game_stats.py
+++ game_stats.py
@@ -9,6 +9,9 @@ class GameStats:
         self.settings = ai_game.settings
         self.reset_stats()
 
+        # High score should never be reset.
+        self.high_score = 0
+
         # Start Alien Invastion in an active state.
         self.game_active = False
 
@@ -16,3 +19,4 @@ class GameStats:
         """Initialize statistics that can change during the game."""
         self.ships_left = self.settings.ship_limit
         self.score = 0
+        self.level = 1
